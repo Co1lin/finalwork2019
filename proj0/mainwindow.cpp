3971cb1f5bb936bc91fd44fefcd5ed6d128374de
enjoycolin <hnpengjj@gmail.com>
2019-11-28 19:29:20 +0800

    Update mainwindow.cpp

--- proj0/mainwindow.cpp
+++ proj0/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "myFun.h"
+#include <QCoreApplication>
 using namespace std;
 
 MainWindow::MainWindow(QWidget *parent)
@@ -24,7 +25,12 @@ void MainWindow::paintEvent(QPaintEvent *)
 {
     //Initial
     ifstream infile;
-    infile.open("/Users/colin/github/finalwork2019/proj0/test0.logo");
+    //"/Users/colin/github/finalwork2019/build-proj0-Desktop_Qt_5_13_1_clang_64bit-Debug/proj0.app/Contents"
+    //QString current_path = QCoreApplication::applicationDirPath();
+    QString current_path = "/Users/colin/github/finalwork2019/proj0";
+    QString input_file = current_path + "/test0.logo";
+    //infile.open("/Users/colin/github/finalwork2019/proj0/test0.logo");
+    infile.open(input_file.toStdString());
     int width = 1920, height = 1080;
     int r = 0, g = 0, b = 0;
     int xpos = 0, ypos = 0;
@@ -38,7 +44,8 @@ void MainWindow::paintEvent(QPaintEvent *)
     mainProcess(infile, painter);
 
     //Save
-    image.save("/Users/colin/github/finalwork2019/proj0/test0.bmp");
+    QString output_file = current_path + "/test0.bmp";
+    image.save(output_file);
 }
 
 int mainProcess(ifstream &infile, QPainter &painter)
